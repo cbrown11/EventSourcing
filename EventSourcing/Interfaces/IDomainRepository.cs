a8e5b112a48a9552c0fea0dd3168b1b4549c03f6
cbrown11 <craig.brown@a4direct.com>
2023-12-29 11:58:40 +0000

    Create IDomainRepository.cs

---
+++ EventSourcing/Interfaces/IDomainRepository.cs
@@ -0,0 +1,18 @@
+namespace EventSourcing.EventSourcing.Interfaces
+{
+    using DomainDrivenDesign.DDD.Interfaces;
+
+    public interface IDomainRepository
+    {
+        Task<IEnumerable<IDomainEvent>> SaveAsync<TAggregate>(TAggregate aggregate, bool isInitial = false, CancellationToken token = default)
+            where TAggregate : IAggregate;
+
+        bool Exists<TResult>(string id)
+            where TResult : IAggregate, new();
+
+        Task<TResult> GetByIdAsync<TResult>(string id, CancellationToken token = default)
+            where TResult : IAggregate, new();
+
+        Task<List<IDomainEvent>> GetEventsForAggregate<TResult>(string aggregateId, CancellationToken token = default);
+    }
+}
